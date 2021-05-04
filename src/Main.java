47b9e83a813fdd6aa2144de9c63720d384eac7c0
hongsu87 <hongsubakgame@naver.com>
2021-05-04 18:36:59 +0900

    .

--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,8 @@ public class Main {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(100);
         }
-        AwesomeSort ShellSort = new BubbleSort();
-        ShellSort.sort(arr);
+        AwesomeSort Sort = new BubbleSort();
+        Sort.sort(arr);
         for (int i = 0; i < arr.length; i++) {
             System.out.printf("%d ", arr[i]);
         }
